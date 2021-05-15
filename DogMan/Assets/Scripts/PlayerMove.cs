a52c129abe108bfbe638763aa7f3bd8e9b5653fc
ArsikUn <lazar10011@gmail.com>
2021-05-15 14:56:04 +0300

    PlayerMove

--- DogMan/Assets/Scripts/PlayerMove.cs
+++ DogMan/Assets/Scripts/PlayerMove.cs
@@ -4,15 +4,60 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+       [SerializeField]
+       private float speed = 6f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+       [SerializeField]
+       private Rigidbody physicsBody = null;
+       
+       [SerializeField] 
+       private Transform groundCheck;
+
+       [SerializeField]
+       private Vector3 _movement;
+
+       public bool isGround ;
+
+       private void Update()
+       {
+              if (Physics2D.Linecast(transform.position,
+                     groundCheck.position,
+                     1 << LayerMask.NameToLayer("Ground")))
+
+              {
+                     isGround = true;
+              }
+              else
+              {
+                     isGround = false;
+              }
+
+              float inputY = 0;
+              if (Input.GetKey(KeyCode.Space)&& isGround==true)
+              {
+                            inputY = 1;
+              }
+              
+              else if (Input.GetKey(KeyCode.DownArrow))
+              {
+                     inputY = -1;
+              }
+              float inputX = 0;
+              if ( Input.GetKey(KeyCode.RightArrow) )
+              {
+                     inputX = 1;
+              }
+              else if (Input.GetKey(KeyCode.LeftArrow))
+              {
+                     inputX = -1;
+              }
+
+              _movement = new Vector3(inputX, inputY, 0).normalized;
+
+       }
+
+       private void FixedUpdate ()
+       {
+              physicsBody.velocity = _movement * speed;
+       }
 }
