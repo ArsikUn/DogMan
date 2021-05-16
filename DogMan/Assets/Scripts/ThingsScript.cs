df9ffa6f8320e6fa1cd2129faca9467b133697d5
Kirill <kirillpazitiv2020@gmail.com>
2021-05-16 20:25:51 +0300

    Ой много чего сделал НО начал делать UI

---
+++ DogMan/Assets/Scripts/ThingsScript.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+
+public class ThingsScript : MonoBehaviour
+{
+     private float speed = 1;
+     [SerializeField] private string name;
+
+     public static Action<string> collect = delegate { };
+     void Update()
+    {
+        transform.Rotate(0,speed,0);
+        
+    }
+
+     void OnTriggerEnter(Collider other)
+     {
+         collect(name);
+     }
+
+   
+
+    
+}
